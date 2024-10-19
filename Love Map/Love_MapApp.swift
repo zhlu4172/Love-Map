d9a5113138af3f370481ff8436cf00e28492d6d6
EMMA LU <zhlu4172@uni.sydney.edu.au>
2024-10-20 00:08:15 +1100

    Initial Commit

---
+++ Love Map/Love_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_MapApp.swift
+//  Love Map
+//
+//  Created by Emma Lu on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Love_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Love MapUITests/Love_MapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Love_MapUITestsLaunchTests.swift
+//  Love MapUITests
+//
+//  Created by Emma Lu on 20/10/2024.
+//
+
+import XCTest
+
+final class Love_MapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
